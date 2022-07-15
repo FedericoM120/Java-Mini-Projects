987cdd15d1f894d042050db61dc32d8fbfb3aec1
FedericoM120 <monteverdi.102@gmail.com>
2022-07-15 12:11:36 -0500

    NumberOfStrings mini project template

--- NumberOfStrings/NumberOfStrings.java
+++ NumberOfStrings/NumberOfStrings.java
@@ -5,6 +5,20 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        while (true){
+            System.out.println("input a string");
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            count++;
+            
+        }
+        System.out.println(count);
+        
+        
 
     }
 }
